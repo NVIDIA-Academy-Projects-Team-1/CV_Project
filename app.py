b78edd555c0d17ca286381fa0dc8a7b4669fb11c
H. Jaeh <huhj98@gmail.com>
2024-06-04 17:44:46 +0900

    annotation generator fixes
    Co-Authored-By: HOUNSIM <160982674+HOUNSIM@users.noreply.github.com>
    Co-Authored-By: Parkshihwan <160465203+Parkshihwan@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,75 +1,17 @@
-# import streamlit as st
-# import numpy as np
-# import pandas as pd
-# import torch as nn          ## 텐서플로우 쓰면 사용 X
-# import tensorflow as tf     ## 파이토치 쓰면 사용 X
-# import time
-# from datetime import datetime
-
-# data = np.array([[1, 2, 3, 4, 5, 6, 7, 8]])
-# index = ['2024.05.31']
-# columns = ['폭행','실신','기물파손','절도','주취','이동상인','몰카','자전거 승차']
-
-# ## 현시간 출력용 데이터 구성
-# today = datetime.now()
-
-# ## 이상행동 보고용 빈 데이타프레임 구성
-# empty_df = pd.DataFrame(data=data, index=index, columns=columns)
-# df_test = pd.DataFrame(np.random.rand(10, 5) * 9 + 1, columns=("이상 행동 %d" % i for i in range(5)))
-
-# ## 비디오 출력용 테스트 데이터 구성
-# video_test = open('test.mp4','rb')
-# video_bytes = video_test.read()
-
-
-# ## 화면 중앙이 아닌 전체에서 보이게 설정
-# st.set_page_config(layout="wide")
-
-# ## 제일 윗단 header 바 부분
-# st.header('지하철 객차 내에 :red[이상행동] 탐색기',divider='rainbow')
-# unsafe_allow_html=True
-
-# ## 사이드에서 입력 및 여러가지 동작 - 1.지하철 호선 선택해서 맟춤 영상 재생
-# st.sidebar.title('Settings')
-
-
-
-# ## section을 3개로 나누어서 3:1:5 비율로 구성
-# section1, section2, section3 = st.columns([3,0.5,5])
-
-# with section1:
-#     st.header('확인중인 영상')
-#     video_container = st.container(border=True)
-#     with video_container:
-#         st.video(video_bytes,
-#                 '''
-#                 <style>
-#                 .video-container {
-#                     width: 90%;
-#                     height: auto;
-#                     margin: 0 auto;
-#                 }
-#                 </style>
-#                 '''
-#             )
-
-# with section2:
-#     pass
-
-# with section3:
-#     st.header('확인된 이상행동')
-#     graph_container = st.container(border=True)
-#     with graph_container:
-#         st.subheader(f'현재 시간 : {today}')
-#         st.write(' ')
-#         st.dataframe(empty_df)
-#         st.bar_chart(empty_df)       ## graph form
-
-#========================================================================================
 ## 로그인 페이지 제작 하여서 로그인시 객체인식 페이지 출력
 import streamlit as st
+import datetime 
+import pandas as pd
+import altair as alt
+import random
+import time 
+from threading import Thread
 
 st.set_page_config(layout='wide',initial_sidebar_state='collapsed')
+
+## 시간 출력하기 위한 필요 변수 설정
+today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+date_placeholder = st.empty()
 
 ## 로그인 동작애 관련한 함수
 def login():
@@ -98,7 +40,10 @@ def main():
         st.session_state['logged_in'] = False
 
     if st.session_state['logged_in']:
-        main_page()
+        if 'show_response' in st.session_state and st.session_state['show_response']:
+            response()
+        else:
+            main_page()
     else:
         login()
 
@@ -106,31 +51,68 @@ def main():
 def main_page():
     st.header('횐영합니다. :red[admin] 님!',divider='rainbow')
     st.subheader('지하철 내에 :blue[이상행동] 탐지')
-    
+    vidoe_paths = ['./test_1.mp4', './test_1.mp4', './test_1.mp4']
+
     ## 영상 객수 추가 하고 싶음 추가 해서 밑에 집어 넣으면 됨
     col1, col2, col3 = st.columns([2,2,2])
     with col1:
-        vidoe_path = './test.mp4'
         st.subheader('1번쨰 CCTV')
-        st.video(vidoe_path,autoplay=True)
+        st.video(vidoe_paths[0],autoplay=True)
 
     with col2:
         st.subheader('2번쨰 CCTV')
-        st.video(vidoe_path,autoplay=True)
+        st.video(vidoe_paths[1],autoplay=True)
 
     with col3:
         st.subheader('3번쨰 CCTV')
-        st.video(vidoe_path,autoplay=True)
+        st.video(vidoe_paths[2],autoplay=True)
 
+    ## 버튼 눌렀을시 결과 출력하는 화면으로 이동하기
     if st.button('결과 보고'):
         st.session_state['show_response'] = True
         st.experimental_rerun()
-    if 'show_response' in st.session_state and st.session_state['show_response']:
-        response()
 
+def timezone():
+    ## 현제 시간 출력하는 코드
+    return date_placeholder.subheader(today)
+    
+def render_chart():
+    ## 데이터 출력용 테스트 데이터 프레임 생성
+    numbers = random.sample(list(range(1,8)),7)
+    df_test = pd.DataFrame(data=numbers,index=['폭행','실신','기물파손','절도','이동상인','몰카','자전거 승차'],columns=['Count'])
+    st.dataframe(df_test)
+    chart = alt.Chart(df_test.reset_index()).mark_bar().encode(x=alt.X('index:N',
+                axis=alt.Axis(labelAngle=0)),y='Count:Q').properties()    ## bar chart 에서 x label 표시를 사용자중점으로 인코딩
+    st.altair_chart(chart, use_container_width=True)    
+
+## 영상 객체 인식 결과 출력하는 페이지
 def response():
-    st.title('response')
+   
+    timezone()
+    
+    st.header('필요한 영상 분석 결과 출력하는 위치')
+    
+    sec1, sec2, sec3 = st.columns([3,3,3])
 
+    with sec1:
+        st.write('1번 영상 결과')
+        render_chart()
+    
+    with sec2:
+        st.write('2번 영상 결과')
+        render_chart()
+
+    with sec3:
+        st.write('3번 영상 결과')
+        render_chart()
+
+    ## 버튼 눌렀을시 영상 출력 화면으로 돌아가기
+    if st.button('CCTV로 돌아가기'):
+        st.session_state['show_response'] = False
+        st.experimental_rerun()
+    
+    time.sleep(1)              ## 업데이트를 몇초 주기로 할건지 설정
+    # st.experimental_rerun()
 
 if __name__ == "__main__":
     main()  
--- dataset_generator/annotation_generator.py
+++ dataset_generator/annotation_generator.py
@@ -3,6 +3,7 @@ import glob
 import os
 from tqdm import tqdm
 from pathlib import Path
+import shutil
 
 
 """
@@ -19,21 +20,13 @@ Label information
 """
 
 annotation_labels = {
-    "폭행" : 0,
-    "실신" : 1,
-    "기물파손" : 2,
-    "절도" : 3,
-    "이동상인" : 4,
-    "몰카" : 5,
-    "자전거 승차" : 6,
-}
-
-json_labels = {
     "assault" : 0,
     "fainting" : 1,
     "property_damage" : 2,
     "theft" : 3,
-    "spy_camera" : 5
+    "merchant" : 4,
+    "spy_camera" : 5,
+    "bicycle" : 6
 }
 
 def generate_annotation(image_parent, annot_parent):
@@ -70,7 +63,15 @@ def generate_annotation(image_parent, annot_parent):
 
                 for annotation in frame['annotations']:
                     label = annotation['label']
-                    category = json_labels[annotation['category']['code']]
+                    try:
+                        category = annotation_labels[annotation['category']['code']]
+                    except:
+                        print(f"No category: {annotation['category']['code']}")
+                        try:
+                            os.remove(image_parent + f"/{id}/frame_{frame['number']}.jpg")
+                            continue
+                        except:
+                            continue
 
                     # Get original coordination
                     x_min = label['x']
@@ -94,77 +95,32 @@ def generate_annotation(image_parent, annot_parent):
                     height_norm = height_new / new_width
 
                     annot.append(f"{category} {x_center_norm} {y_center_norm} {width_norm} {height_norm}")
-                    
-                if not os.path.exists(image_parent + f'/{id}_annotation'):
-                    os.makedirs(image_parent + f'/{id}_annotation')
-                    with open(image_parent + f"/{id}_annotation/frame_{frame['number']}.txt", "w") as txt:
-                        for i in range(len(annot)):
-                            txt.write(f"{annot[i]}\n")
-                else:
-                    with open(image_parent + f"/{id}_annotation/frame_{frame['number']}.txt", "w") as txt:
+                
+                if len(annot) != 0:
+                    if not os.path.exists(image_parent + f'/annotation_{id}'):
+                        os.makedirs(image_parent + f'/annotation_{id}')
+                        with open(image_parent + f"/annotation_{id}/frame_{frame['number']}.txt", "w") as txt:
                             for i in range(len(annot)):
                                 txt.write(f"{annot[i]}\n")
+                    else:
+                        with open(image_parent + f"/annotation_{id}/frame_{frame['number']}.txt", "w") as txt:
+                                for i in range(len(annot)):
+                                    txt.write(f"{annot[i]}\n")
 
 
+def delete_annot_file(image_parent, annot_parent):
+    annot_file_ids = sorted([Path(file).stem.split('_')[1] for file in glob.glob(annot_parent + '/*')])
 
+    for id in tqdm(annot_file_ids):
+        annot_files = sorted(glob.glob(image_parent + f'/annotation_{id}/*'))
 
-    ###############################################################################################
-    # image_frames = [int(os.path.splitext(os.path.basename(file))[0].split('_')[1]) for file in files if os.path.isfile(file)]
-    # image_frames.sort()
-
-    # with open('/Users/jaeh/Documents/NVIDIA_AI_Academy/CV_Project/test_data/annotation_2120699.json') as f:
-    #     data = json.load(f)
-
-    #     for frame in data['frames']:
-    #         if frame['number'] in image_frames:
-    #             print(frame['number'])
-    #             annot = []
-    #             for annotation in frame['annotations']:
-    #                 label = annotation['label']
-    #                 x_min = label['x']
-    #                 y_min = label['y']
-    #                 width = label['width']
-    #                 height = label['height']
-                    
-    #                 # convert to center coordinates
-    #                 x_center = x_min + width / 2
-    #                 y_center = y_min + height / 2
-                    
-    #                 # scale the center coordinates and dimensions
-    #                 x_center_new = x_center * new_width / original_width
-    #                 y_center_new = y_center * new_height / original_height
-    #                 width_new = width * new_width / original_width
-    #                 height_new = height * new_height / original_height
-
-    #                 x_center_norm = x_center_new / new_width
-    #                 y_center_norm = y_center_new / new_height
-    #                 width_norm = width_new / new_width
-    #                 height_norm = height_new / new_width
-
-    #                 annot.append(f"0 {x_center_norm} {y_center_norm} {width_norm} {height_norm}")
-                    
-
-    #             with open(f"/Users/jaeh/Documents/NVIDIA_AI_Academy/CV_Project/test_data/generated_annotations/frame_{frame['number']}.txt", "w") as txt:
-    #                 for i in range(len(annot)):
-    #                     if i == (len(annot) - 1):
-    #                         txt.write(f"{annot[i]}")
-    #                     else:
-    #                         txt.write(f"{annot[i]}\n")
-
-
-def remove_file_annot(image_folder, annot_folder):
-    image_files = sorted(glob.glob(image_folder))
-    annot_files = sorted(glob.glob(annot_folder))
-
-    for image_file, annot_file in tqdm(zip(image_files, annot_files), total = len(image_files)):
-        with open(annot_file) as f2:
-            # label_list = [[[int(value)] for value in item.split()] for item in f2.readlines()]
-            labels = [int(item.split()[0]) for item in f2.readlines()]
-            # print(labels)
-            if 5 not in labels:
-                # print(image_file, annot_file)
-                os.remove(image_file)
-                os.remove(annot_file)
+        for file in annot_files:
+            filename = Path(file).stem
+            if Path(image_parent + f'/{id}/{filename}.jpg').is_file():
+                continue
+            else:
+                print(f"image : {filename}.jpg does not exist")
+                os.remove(file)
 
 
 def match_files(image_folder, annot_folder):
@@ -214,10 +170,54 @@ def add_annot_info_file(image_folder, annot_folder, original_label, new_label):
                     f.write(line + "\n")
 
 
+def relabel():
+    annot_files = sorted(glob.glob('D:/Downloads/bicycle_annotated/*'))
+
+    # Relabel annotation file
+    for annot_file in annot_files:
+        annotation = []
+        with open(annot_file, "r") as f:
+            labels = [item.strip() for item in f.readlines()]
+            for label in labels:
+                annotation.append(label.replace('7', '6', 1))
+
+        with open(annot_file, "w") as f:
+            for line in annotation:
+                f.write(line + "\n")
+
+
+def relocate_image_annot(image_parent, annot_parent):
+    num = 1
+    image_folders = sorted(glob.glob(os.path.join(image_parent, '*')))
+    annot_folders = sorted(glob.glob(os.path.join(annot_parent, '*')))
+
+
+    image_target = 'C:/Users/hancom09/Downloads/'
+    annot_target = 'C:/Users/hancom09/Downloads/'
+    
+    for (image_folder, annot_folder) in zip(image_folders, annot_folders):
+        image_files = sorted(glob.glob(os.path.join(image_folder, '*')))
+        annot_files = sorted(glob.glob(os.path.join(annot_folder, '*')))
+
+        for (image_file, annot_file) in zip(image_files, annot_files):
+            new_image_path = os.path.join(image_target, f'{num}.jpg')
+            new_annot_path = os.path.join(annot_target, f'{num}.txt')
+
+            print(3)
+            os.rename(image_file, new_image_path)
+            os.rename(annot_file, new_annot_path)
+            num += 1
+
 
 if __name__ == "__main__":
-    generate_annotation('C:/Users/hancom09/Downloads/CV_CCTV/CV_Project/test_data', 'C:/Users/hancom09/Downloads/CV_CCTV/CV_Project/test_data/annotation')
+    # generate_annotation('D:/Datasets', 'D:/Datasets/annotation')
+
+    # generate_annotation('C:/Users/hancom09/Downloads/CV_CCTV/CV_Project/test_data', 'C:/Users/hancom09/Downloads/CV_CCTV/CV_Project/test_data/annotation')
 
     # remove_file_annot('/Users/jaeh/Downloads/train_img/train_img/*', '/Users/jaeh/Downloads/train_label/train_label/*')
 
     # add_annot_info_file('/Users/jaeh/Downloads/bicycle/*', '/Users/jaeh/Downloads/bicycle_annotated/*', 1, 7)
+
+    # relabel()
+
+    relocate_image_annot('C:\\Users/hancom09/Downloads/CV_CCTV/CV_Project/test_data/test_dataset_folder/train/images', 'C:/Users/hancom09/Downloads/CV_CCTV/CV_Project/test_data/test_dataset_folder/train/labels')
